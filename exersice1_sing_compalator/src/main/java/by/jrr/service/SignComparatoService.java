8019812bfdc7fb78611ef9fe1fd53c065d79fae9
“tatianamih” <tmihalchenko66@gmail.com>
2020-09-01 01:52:59 +0300

    task about sing compalator with test sonar widget and pom.xml

---
+++ exersice1_sing_compalator/src/main/java/by/jrr/service/SignComparatoService.java
@@ -0,0 +1,20 @@
+package by.jrr.service;
+
+import by.jrr.bean.SignComparato;
+
+public class SignComparatoService {
+
+    public SignComparatoService() {
+// no args constructor
+    }
+
+       public String determineTheSign(SignComparato signComparato) {
+        if (signComparato.getNumber() > 0) {
+            return "Number is positive";
+        } else if (signComparato.getNumber() < 0) {
+            return "Number is negative";
+        } else {
+            return "Number is equal to zero";
+        }
+    }
+}
